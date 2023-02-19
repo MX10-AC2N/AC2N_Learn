3614ffe2ba6e611ddee2b8d624792c355ef41967
MX10-AC2N <62643837+MX10-AC2N@users.noreply.github.com>
2023-02-19 15:22:00 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,24 @@
 
+const paymentForm = document.getElementById('payment-form');
+const paymentAmount = document.getElementById('payment-amount');
+const paymentCurrency = document.getElementById('payment-currency');
+const paymentSubmit = document.getElementById('payment-submit');
+
+paymentForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    // Payment logic here
+});
+
+paymentAmount.addEventListener('input', (event) => {
+    // Payment amount logic here
+});
+
+paymentCurrency.addEventListener('change', (event) => {
+    // Payment currency logic here
+});
+
+paymentSubmit.addEventListener('click', (event) => {
+    // Payment submit logic here
+});
+﻿
